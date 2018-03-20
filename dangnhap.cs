3c4f05dca70b23c4abada5ada1e0d2df8fc15681
duchoa97 <duchoa97@gmail.com>
2018-03-20 15:55:48 +0700

    Tạo form Đăng nhập

---
+++ dangnhap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Quản_lý_bán_hàng
+{
+    public partial class Formdangnhap : Form
+    {
+        
+        public Formdangnhap()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
