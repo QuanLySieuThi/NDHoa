a2325bae58edfa39f8cd7fa5001f9dc236bdb7fd
duchoa97 <duchoa97@gmail.com>
2018-03-30 15:58:11 +0700

    Hoàn thiện form Đăng nhập, tạo form Đổi mk
    Tạo form đổi mật khẩu

---
+++ Đổi MK.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Quản_lý_bán_hàng
+{
+    public partial class formdoipass : Form
+    {
+        public formdoipass()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-NL36GS2\SQLEXPRESS;Initial Catalog=Quanlybanhang;Integrated Security=True");
+    
+              
+        }
+       
